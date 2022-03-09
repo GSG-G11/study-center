c0f675eaf0adbce241984913b75ff917354db3d7
israaHamdi <israaaburayya2012@gmail.com>
2022-03-09 14:06:10 +0200

    edit name database url

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 let DB_URL = '';
 
 if (process.env.NODE_ENV === 'production') {
-  DB_URL = process.env.DB_PRODUCTION;
+  DB_URL = process.env.DATABASE_URL;
 } else if (process.env.NODE_ENV === 'development') {
   DB_URL = process.env.DB_URL;
 } else if (process.env.NODE_ENV === 'test') {
